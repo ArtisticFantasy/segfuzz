d8037d5800bcca1d257e047050b2c6407497c680
Artistic Fantasy <1694888082@qq.com>
2023-11-29 23:04:25 -0800

    build

--- scripts/llvm/_envsetup.sh
+++ scripts/llvm/_envsetup.sh
@@ -2,5 +2,7 @@
 
 __export_envvar "LLVM" "$TOOLCHAINS_DIR/llvm"
 __append_path "$LLVM_INSTALL/bin"
+__append_path "$CMAKE_INSTALL/bin"
+__append_path "$NINJA_INSTALL/bin"
 export CLANG="$LLVM_INSTALL/bin/clang"
 export LLVM_VERSION="llvmorg-12.0.1"
